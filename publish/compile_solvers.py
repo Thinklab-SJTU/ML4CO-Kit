ffe348d2c9f6a6feae1b18e456b4000a78e600ba
heatingma <115260102+heatingma@users.noreply.github.com>
2024-02-11 17:46:35 +0800

    data4co-0.0.1a5

---
+++ publish/compile_solvers.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+
+# pyconcorde
+ori_dir = os.getcwd()
+os.chdir('data4co/solver/tsp/pyconcorde')
+os.system("python ./setup.py build_ext --inplace")
+os.chdir(ori_dir)
+
+# KaMIS
+shutil.copytree('data4co/solver/mis/kamis-source/', 
+                'data4co/solver/mis/KaMIS/tmp_build/')
+ori_dir = os.getcwd()
+os.chdir('data4co/solver/mis/KaMIS/tmp_build')
+os.system("bash cleanup.sh")
+os.system("bash compile_withcmake.sh")
+os.chdir(ori_dir)
+shutil.copytree('data4co/solver/mis/KaMIS/tmp_build/deploy/', 
+                'data4co/solver/mis/KaMIS/deploy/')
+shutil.rmtree('data4co/solver/mis/KaMIS/tmp_build/')
