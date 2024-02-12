68968cb9c65432756c09693231d9cc8aed93d965
heatingma <115260102+heatingma@users.noreply.github.com>
2024-02-12 16:24:15 +0800

    update MISSolver

--- data4co/solver/mis/base.py
+++ data4co/solver/mis/base.py
@@ -1,4 +1,7 @@
+import os
+import pickle
 import pathlib
+import numpy as np
 import networkx as nx
 
 
@@ -7,7 +10,57 @@ class MISSolver:
         self.solver_type = None
         self.weighted = None
         self.time_limit = 60.0
-    
+        self.nodes_num = None
+        self.node_labels = None
+        self.edges = None
+        
+    def from_folder(self, folder: str, weighted: bool=False):
+        self.folder = folder
+        self.solve_folder = os.path.join(self.folder, "solve")
+        read_label = True if os.path.exists(self.solve_folder) else False
+        files = os.listdir(folder)
+        self.nodes_num = list()
+        self.node_labels = list()
+        self.edges = list()
+        for filename in files:
+            if not filename.endswith(".gpickle"):
+                continue
+            file_path = os.path.join(self.folder, filename)
+            with open(file_path, "rb") as f:
+                graph = pickle.load(f)
+            graph: nx.Graph
+            # nodes num
+            nodes_num = graph.number_of_nodes()
+            # node lables
+            if not read_label:
+                node_labels = [_[1] for _ in graph.nodes(data='label')]
+                if node_labels is not None and node_labels[0] is not None:
+                    node_labels = np.array(node_labels, dtype=np.int64)
+                else:
+                    node_labels = np.zeros(nodes_num, dtype=np.int64)
+                    edges = np.array(graph.edges, dtype=np.int64)
+            else:
+                solve_filename = filename.replace('.gpickle', \
+                    f"_{'weighted' if weighted else 'unweighted'}.result")
+                solve_file_path = os.path.join(self.solve_folder, solve_filename)
+                with open(solve_file_path, 'r') as f:
+                    node_labels = [int(_) for _ in f.read().splitlines()]
+                node_labels = np.array(node_labels, dtype=np.int64)
+                if node_labels.shape[0] != nodes_num:
+                    message = "The number of nodes in the solution result does not match the number " 
+                    message += "of nodes in the problem. Please check the solution result."
+                    raise ValueError(message)
+            # edges
+            edges = np.array(graph.edges, dtype=np.int64)
+            edges = np.concatenate([edges, edges[:, ::-1]], axis=0)
+            self_loop = np.arange(nodes_num).reshape(-1, 1).repeat(2, axis=1)
+            edges = np.concatenate([edges, self_loop], axis=0)
+            edges = edges.T
+            # add to list
+            self.nodes_num.append(nodes_num)
+            self.node_labels.append(node_labels)
+            self.edges.append(edges)
+         
     @staticmethod
     def __prepare_graph(g: nx.Graph, weighted = False):
         raise NotImplementedError("__prepare_graph is required to implemented in subclass")
