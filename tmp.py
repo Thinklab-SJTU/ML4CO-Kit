9db5219014491b3169ed70ffc96ea9110f3400c7
ubuntu <ubuntu@localhost.localdomain>
2024-03-31 01:24:21 +0800

    update README.md

--- tmp.py
+++
@@ -1,6 +0,0 @@
-from data4co.evaluate import TSPLIBOriginEvaluator
-from data4co.solver import TSPLKHSolver, TSPConcordeSolver
-
-lkh_solver = TSPLIBOriginEvaluator()
-eva = TSPLIBOriginEvaluator()
-eva.evaluate(lkh_solver, norm="EUC_2D")
