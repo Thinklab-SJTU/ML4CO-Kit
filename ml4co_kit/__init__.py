ce07403667d4a10b7dc16bc243f21206b7df6a35
heatingma <heatingma@sjtu.edu.cn>
2024-08-26 15:08:40 +0800

    ML4CO-Kit 0.1.0

--- ml4co_kit/__init__.py
+++ ml4co_kit/__init__.py
@@ -34,5 +34,5 @@ if found_pytorch_lightning is not None:
     from .learning.utils import points_to_distmat, sparse_points
 
 
-__version__ = "0.0.9"
+__version__ = "0.1.0"
 __author__ = "SJTU-ReThinkLab"
