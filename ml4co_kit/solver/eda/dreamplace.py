8fd933de6dedd6c9b28bf00256a77376f9f3b6de
heatingma <heatingma@sjtu.edu.cn>
2026-05-27 19:34:50 +0800

    Update License

--- ml4co_kit/solver/eda/dreamplace.py
+++ ml4co_kit/solver/eda/dreamplace.py
@@ -6,7 +6,6 @@ DreamPlace solver for EDA problems.
 # ML4CO-Kit is licensed under Mulan PSL v2.
 # You may obtain a copy of Mulan PSL v2 at:
 # http://license.coscl.org.cn/MulanPSL2
-#
 # THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
 # EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
